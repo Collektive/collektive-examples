47083fbbedb910289aa5deb7a19be92be48241d8
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2023-10-29 17:32:02 +0100

    chore: align collektive alchemist integration version

--- src/main/kotlin/it/unibo/collektive/Aggregate.kt
+++ src/main/kotlin/it/unibo/collektive/Aggregate.kt
@@ -1,17 +1,21 @@
 package it.unibo.collektive
 
+import it.unibo.collektive.aggregate.AggregateContext
+import it.unibo.collektive.aggregate.AggregateResult
+import it.unibo.collektive.aggregate.aggregate
+import it.unibo.collektive.aggregate.ops.share
 import it.unibo.collektive.alchemist.device.CollektiveDevice
 import it.unibo.collektive.alchemist.device.DistanceSensor
 import it.unibo.collektive.field.Field
 import it.unibo.collektive.field.min
-import it.unibo.collektive.stack.Path
+import it.unibo.collektive.state.State
 
 class Aggregate(private val device: CollektiveDevice<*>) {
     private val nodeId = device.node.id
-    private var state = emptyMap<Path, Any?>()
+    private var state: Set<State<*>> = setOf()
 
-    fun entrypoint(): AggregateContext.AggregateResult<Double> = with(device) {
-        val gradientResult = aggregate(IntId(nodeId), receive(), state) {
+    fun entrypoint(): AggregateResult<Double> = with(device) {
+        val gradientResult = aggregate(IntId(nodeId), read(), state) {
             gradient(nodeId == 0)
         }
         state = gradientResult.newState
@@ -28,7 +32,7 @@ operator fun Field<Double>.plus(field: Field<Double>): Field<Double> {
 }
 
 context(AggregateContext, DistanceSensor)
-fun gradient(source: Boolean) = sharing(Double.POSITIVE_INFINITY) { dist ->
+fun gradient(source: Boolean) = share(Double.POSITIVE_INFINITY) { dist ->
     val paths = distances() + dist
     val minByPath = paths.min(includingSelf = false)?.value // field to map, excluding local
     when {
