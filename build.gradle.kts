421ec280585de74ac8e931dec3afa245f8ca053c
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-01-22 22:23:53 +0100

    feat: launch incarnation with yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
-/*
- * DEFAULT GRADLE BUILD FOR ALCHEMIST SIMULATOR
- */
+import java.util.Locale
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+apply(plugin = libs.plugins.kotlin.jvm.get().pluginId)
 
 plugins {
     application
@@ -15,7 +16,6 @@ repositories {
 }
 
 dependencies {
-    // Check the catalog at gradle/libs.versions.gradle
     implementation(libs.bundles.alchemist)
     implementation(libs.collektive.integration)
     implementation(libs.collektive)
@@ -25,78 +25,56 @@ multiJvm {
     jvmVersionForCompilation.set(latestJava)
 }
 
-kotlin {
-    target {
-        compilations.all {
-            kotlinOptions {
-                freeCompilerArgs += listOf("-Xcontext-receivers")
-            }
-        }
-    }
-}
-
-val batch: String by project
-val maxTime: String by project
-
 val alchemistGroup = "Run Alchemist"
-/*
- * This task is used to run all experiments in sequence
- */
+
 val runAll by tasks.register<DefaultTask>("runAll") {
     group = alchemistGroup
     description = "Launches all simulations"
 }
+
+fun String.capitalizeString(): String = this.replaceFirstChar {
+    if (it.isLowerCase()) it.titlecase(
+        Locale.getDefault()
+    ) else it.toString()
+}
+
 /*
  * Scan the folder with the simulation files, and create a task for each one of them.
  */
+
+println("PATH ${rootProject.rootDir.path}")
 File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
-    ?.filter { it.extension == "yml" } // pick all yml files in src/main/yaml
-    ?.sortedBy { it.nameWithoutExtension } // sort them, we like reproducibility
-    ?.forEach {
-        // one simulation file -> one gradle task
-        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.uppercase()}") {
-            group = alchemistGroup // This is for better organization when running ./gradlew tasks
-            description = "Launches simulation ${it.nameWithoutExtension}" // Just documentation
-            mainClass.set("it.unibo.alchemist.Alchemist") // The class to launch
-            classpath = sourceSets["main"].runtimeClasspath // The classpath to use
-            // Uses the latest version of java
+    .orEmpty()
+    .apply { check(isNotEmpty()) }
+    .filter { it.extension == "yaml" }
+    .sortedBy { it.nameWithoutExtension }
+    .forEach {
+        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalizeString()}") {
             javaLauncher.set(
                 javaToolchains.launcherFor {
                     languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
-                },
+                }
             )
-            // These are the program arguments
-            args("run", it.absolutePath, "--override")
-            if (System.getenv("CI") == "true" || batch == "true") {
-                // If it is running in a Continuous Integration environment, use the "headless" mode of the simulator
-                // Namely, force the simulator not to use graphical output.
-                args(
-                    """
-                        terminate:
-                        - type: AfterTime
-                          parameters: $maxTime
-                    """.trimIndent(),
-                )
-            } else {
-                // A graphics environment should be available, so load the effects for the UI from the "effects" folder
-                // Effects are expected to be named after the simulation file
-                args(
-                    """
-                        launcher:
-                          type: SingleRunSwingUI
-                          parameters:
-                            graphics: effects/${it.nameWithoutExtension}.json
-                    """,
-                )
+            group = alchemistGroup
+            description = "Launches simulation ${it.nameWithoutExtension}"
+            mainClass.set("it.unibo.alchemist.Alchemist")
+            classpath = sourceSets["main"].runtimeClasspath
+            val exportsDir = File("${projectDir.path}/build/exports/${it.nameWithoutExtension}")
+            doFirst {
+                if (!exportsDir.exists()) {
+                    exportsDir.mkdirs()
+                }
             }
+            args("run", it.absolutePath)
+            args(
+                "--override",
+                "{ launcher: { parameters: { graphics: \"effects/${it.nameWithoutExtension}.json\" } } }"
+            )
+            outputs.dir(exportsDir)
         }
-        // task.dependsOn(classpathJar) // Uncomment to switch to jar-based classpath resolution
         runAll.dependsOn(task)
     }
 
-tasks.withType<Tar>().configureEach {
-    duplicatesStrategy = DuplicatesStrategy.WARN
-}
-tasks.withType<Zip>().configureEach {
-    duplicatesStrategy = DuplicatesStrategy.WARN
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.freeCompilerArgs = listOf("-Xcontext-receivers")
 }
