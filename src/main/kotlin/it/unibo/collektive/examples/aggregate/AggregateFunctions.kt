ee5b884fbe5c0f3eef7c9707b90e54fb9138b70b
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-01-30 17:51:33 +0100

    feat: update Aggregate functions with new semantics

--- src/main/kotlin/it/unibo/collektive/examples/aggregate/AggregateFunctions.kt
+++ src/main/kotlin/it/unibo/collektive/examples/aggregate/AggregateFunctions.kt
@@ -1,29 +1,27 @@
 package it.unibo.collektive.examples.aggregate
 
-import it.unibo.collektive.ID
-import it.unibo.collektive.IntId
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.operators.neighboring
+import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
 import it.unibo.collektive.aggregate.api.operators.share
 import it.unibo.collektive.alchemist.device.DistanceSensor
-import it.unibo.collektive.field.Field.Companion.hoodInto
+import it.unibo.collektive.field.Field.Companion.hood
 import it.unibo.collektive.field.plus
 import kotlin.Double.Companion.POSITIVE_INFINITY
 
 /**
  * TODO.
  */
-fun Aggregate.neighborCounter(): Int = neighboring(1).hoodInto(0) { acc, _ -> acc + 1 }
+fun Aggregate<Int>.neighborCounter(): Int = neighboringViaExchange(1).hood(0) { acc, _ -> acc + 1 }
 
 /**
  * TODO.
  */
 context(DistanceSensor)
-fun Aggregate.gradient(id: ID): Double {
+fun Aggregate<Int>.gradient(id: Int): Double {
     val paths = distances()
     return share(POSITIVE_INFINITY) { dist ->
         val minByPath: Double = (paths + dist).excludeSelf().map { it.value }.minOrNull() ?: POSITIVE_INFINITY
-        if (id == IntId(0)) {
+        if (id == 0) {
             0.0
         } else {
             minByPath
@@ -35,4 +33,4 @@ fun Aggregate.gradient(id: ID): Double {
  * TODO.
  */
 context(DistanceSensor)
-fun Aggregate.entrypoint(): Double = gradient(localId)
+fun Aggregate<Int>.entrypoint(): Double = gradient(localId)
