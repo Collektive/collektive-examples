05c9e39b78c7ac92ff7d8208c75cb241abf49498
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-02 22:07:16 +0100

    feat: create simulation entrypoints

--- src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
+++ src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
@@ -1,18 +1,17 @@
-package it.unibo.collektive.examples.aggregate
+package it.unibo.collektive.examples.entrypoint
 
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.operators.neighboringViaExchange
 import it.unibo.collektive.alchemist.device.DistanceSensor
 import it.unibo.collektive.examples.gradient.gradient
-import it.unibo.collektive.field.Field.Companion.hood
+import it.unibo.collektive.examples.neighbors.neighborCounter
 
 /**
- * Extension function to evaluate the number of neighbors of a node in an [Aggregate] context.
- */
-fun Aggregate<Int>.neighborCounter(): Int = neighboringViaExchange(1).hood(0) { acc, _ -> acc + 1 }
-
-/**
- * The entrypoint of the simulation.
+ * The entrypoint of the simulation running a gradient.
  */
 context(DistanceSensor)
-fun Aggregate<Int>.entrypoint(): Double = gradient()
+fun Aggregate<Int>.gradientEntrypoint(): Double = gradient()
+
+/**
+ * The entrypoint of the simulation running a counter of neighbors.
+ */
+fun Aggregate<Int>.neighborCounterEntrypoint(): Int = neighborCounter()
