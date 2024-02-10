ade44f73e45f581b8c4a64a24423a7a726744f5b
Angela Cortecchia <angela.cortecchia@studio.unibo.it>
2024-02-10 15:36:39 +0100

    feat: create new tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.awt.GraphicsEnvironment
+import java.io.ByteArrayOutputStream
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.util.Locale
 
@@ -18,18 +20,44 @@ repositories {
 dependencies {
     implementation(libs.bundles.alchemist)
     implementation(libs.bundles.collektive)
+    if (!GraphicsEnvironment.isHeadless()) {
+        implementation("it.unibo.alchemist:alchemist-swingui:${libs.versions.alchemist.get()}")
+    }
 }
 
 multiJvm {
     jvmVersionForCompilation.set(latestJava)
 }
 
-val alchemistGroup = "Run Alchemist"
-
-val runAll by tasks.register<DefaultTask>("runAll") {
-    group = alchemistGroup
-    description = "Launches all simulations"
+// Heap size estimation for batches
+val maxHeap: Long? by project
+val heap: Long = maxHeap ?: if (System.getProperty("os.name").lowercase().contains("linux")) {
+    ByteArrayOutputStream().use { output ->
+        exec {
+            executable = "bash"
+            args = listOf("-c", "cat /proc/meminfo | grep MemAvailable | grep -o '[0-9]*'")
+            standardOutput = output
+        }
+        output.toString().trim().toLong() / 1024
+    }.also { println("Detected ${it}MB RAM available.") } * 9 / 10
+} else {
+    // Guess 16GB RAM of which 2 used by the OS
+    14 * 1024L
 }
+val taskSizeFromProject: Int? by project
+val taskSize = taskSizeFromProject ?: 512
+val threadCount = maxOf(1, minOf(Runtime.getRuntime().availableProcessors(), heap.toInt() / taskSize))
+
+val runAllGraphic by tasks.register<DefaultTask>("runAllGraphic") {
+    group = alchemistGroup
+    description = "Launches all simulations with the graphic subsystem enabled"
+}
+val runAllBatch by tasks.register<DefaultTask>("runAllBatch") {
+    group = alchemistGroup
+    description = "Launches all experiments"
+}
+
+val alchemistGroup = "Run Alchemist"
 
 fun String.capitalizeString(): String =
     this.replaceFirstChar {
@@ -42,34 +70,43 @@ fun String.capitalizeString(): String =
         }
     }
 
-/*
- * Scan the folder with the simulation files, and create a task for each one of them.
- */
 File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
-    .orEmpty()
-    .filter { it.extension == "yaml" }
-    .sortedBy { it.nameWithoutExtension }
-    .forEach {
-        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalizeString()}") {
+    ?.filter { it.extension == "yml" }
+    ?.sortedBy { it.nameWithoutExtension }
+    ?.forEach {
+        fun basetask(name: String, additionalConfiguration: JavaExec.() -> Unit = {}) = tasks.register<JavaExec>(name) {
+            group = alchemistGroup
+            description = "Launches graphic simulation ${it.nameWithoutExtension}"
+            mainClass.set("it.unibo.alchemist.Alchemist")
+            classpath = sourceSets["main"].runtimeClasspath
+            args("run", it.absolutePath)
             javaLauncher.set(
                 javaToolchains.launcherFor {
                     languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
                 },
             )
-            group = alchemistGroup
-            description = "Launches simulation ${it.nameWithoutExtension}"
-            mainClass.set("it.unibo.alchemist.Alchemist")
-            classpath = sourceSets["main"].runtimeClasspath
-            val exportsDir = File("${projectDir.path}/build/exports/${it.nameWithoutExtension}")
-            doFirst {
-                if (!exportsDir.exists()) {
-                    exportsDir.mkdirs()
-                }
+            if (System.getenv("CI") == "true") {
+                args("--override", "terminate: { type: AfterTime, parameters: [20] } ")
+            } else {
+                this.additionalConfiguration()
             }
-            args("run", it.absolutePath)
-            outputs.dir(exportsDir)
         }
-        runAll.dependsOn(task)
+        val capitalizedName = it.nameWithoutExtension.capitalizeString()
+        val graphic by basetask("run${capitalizedName}Graphic") {
+            args(
+                "--override",
+                "monitors: { type: SwingGUI, parameters: { graphics: effects/${it.nameWithoutExtension}.json } }",
+                "--override",
+                "launcher: { parameters: { batch: [], autoStart: false } }",
+            )
+        }
+        runAllGraphic.dependsOn(graphic)
+        val batch by basetask("run${capitalizedName}Batch") {
+            description = "Launches batch experiments for $capitalizedName"
+            maxHeapSize = "${minOf(heap.toInt(), Runtime.getRuntime().availableProcessors() * taskSize)}m"
+            File("data").mkdirs()
+        }
+        runAllBatch.dependsOn(batch)
     }
 
 tasks.withType(KotlinCompile::class).all {
