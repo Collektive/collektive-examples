9afab2eb8b2f340b9dad2bb136b3246e3ffd3bcd
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-02-24 14:57:03 +0100

    feat: add channel example

---
+++ src/main/kotlin/it/unibo/collektive/examples/channel/Channel.kt
@@ -0,0 +1,40 @@
+package it.unibo.collektive.examples.channel
+
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.aggregate.api.impl.project
+import it.unibo.collektive.aggregate.api.operators.share
+import it.unibo.collektive.alchemist.device.DistanceSensor
+import it.unibo.collektive.examples.gradient.gradient
+import it.unibo.collektive.field.Field.Companion.fold
+import kotlin.io.path.fileVisitor
+
+context(DistanceSensor)
+fun Aggregate<Int>.channel(source: Boolean, destination: Boolean, width: Double): Boolean {
+    val distancesToSource = distanceTo(source)
+    val distanceToDestination = distanceTo(destination)
+    val dBetween = distanceBetween(source, destination)
+    return !((distancesToSource + distanceToDestination).isInfinite() && dBetween.isInfinite()) &&
+            distancesToSource + distanceToDestination <= dBetween + width
+}
+
+context(DistanceSensor)
+fun Aggregate<Int>.broadcast(source: Boolean, value: Double, accumulator: (Double) -> Double): Double =
+    share(Double.POSITIVE_INFINITY to value) { field ->
+        val dist = distances()
+        when {
+            source -> 0.0 to value
+            else -> {
+                val resultField = dist.alignedMap(field) { distField, (dist2, value) -> distField + dist2 to accumulator(value) }
+                resultField.fold(Double.POSITIVE_INFINITY to Double.POSITIVE_INFINITY) { acc, value ->
+                    if (value.first < acc.first) value else acc
+                }
+            }
+        }
+    }.second
+
+context(DistanceSensor)
+fun Aggregate<Int>.distanceBetween(source: Boolean, destination: Boolean): Double =
+    broadcast(source, distanceTo(destination)) { it }
+
+context(DistanceSensor)
+fun Aggregate<Int>.distanceTo(target: Boolean): Double = gradient(target)
--- src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
+++ src/main/kotlin/it/unibo/collektive/examples/entrypoint/SimulationEntrypoint.kt
@@ -4,6 +4,7 @@ import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.alchemist.device.DistanceSensor
 import it.unibo.collektive.examples.gradient.gradient
 import it.unibo.collektive.examples.neighbors.neighborCounter
+import it.unibo.collektive.examples.channel.channel
 
 /**
  * The entrypoint of the simulation running a gradient.
@@ -15,3 +16,6 @@ fun Aggregate<Int>.gradientEntrypoint(): Double = gradient(localId == 0)
  * The entrypoint of the simulation running a counter of neighbors.
  */
 fun Aggregate<Int>.neighborCounterEntrypoint(): Int = neighborCounter()
+
+context(DistanceSensor)
+fun Aggregate<Int>.channelEntrypoint(): Boolean = channel(localId == 0, localId == 99, 1.0)
