04b754858f3287d53feffc7e26fc7fec02cb8688
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-09 12:23:31 +0200

    chore: add hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.enterprise") version "3.19.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.22"
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
@@ -19,6 +20,14 @@ develocity {
         termsOfUseAgree = "yes"
         uploadInBackground = !System.getenv("CI").toBoolean()
     }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    preCommit {
+        tasks("detektAll", "ktlintCheck")
+    }
+    createHooks(overwriteExisting = true)
 }
 
 include("distributed", "simulation")
