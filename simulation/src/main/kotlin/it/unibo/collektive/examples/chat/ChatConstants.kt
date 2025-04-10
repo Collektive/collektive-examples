623d5d8b40fd6cc53e582d8a460b1fe6f8a69a89
Luca Marchi <luca1010.marchi@gmail.com>
2025-04-10 15:23:03 +0200

    refactor: removed public to constants

--- simulation/src/main/kotlin/it/unibo/collektive/examples/chat/ChatConstants.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/chat/ChatConstants.kt
@@ -3,8 +3,8 @@ package it.unibo.collektive.examples.chat
 /**
  * Maximum distance at which the message is clearly received.
  */
-public const val REACHABLE: Double = 5.0
+const val REACHABLE: Double = 5.0
 /**
  * Maximum distance at which the message can still be faintly perceived.
  */
-public const val THRESHOLD: Double = 10.0
+const val THRESHOLD: Double = 10.0
--- simulation/src/main/kotlin/it/unibo/collektive/examples/chat/ChatUtils.kt
+++
@@ -1,6 +0,0 @@
-package it.unibo.collektive.examples.chat
-
-
-
-
-
