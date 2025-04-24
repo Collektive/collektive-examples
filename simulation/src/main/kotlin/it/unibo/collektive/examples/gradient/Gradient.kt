0ab7790929392816997e0aa09b9ce19a3ae427c6
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-24 15:16:29 +0200

    fix: remove use of deprecated functions

--- simulation/src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
@@ -1,21 +1,22 @@
 package it.unibo.collektive.examples.gradient
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
+import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.share
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
-import it.unibo.collektive.field.operations.min
 import it.unibo.collektive.stdlib.doubles.FieldedDoubles.plus
+import it.unibo.collektive.stdlib.fields.minValue
 import kotlin.Double.Companion.POSITIVE_INFINITY
 
 /**
  * Extension function to evaluate the gradient in an [Aggregate] context.
  */
 fun Aggregate<Int>.gradient(distanceSensor: CollektiveDevice<*>, source: Boolean): Double = share(POSITIVE_INFINITY) {
-    val dist = with(distanceSensor) { distances() }
+    val dist: Field<Int, Double> = with(distanceSensor) { distances() }
     when {
         source -> 0.0
-        else -> (it + dist).min(POSITIVE_INFINITY)
+        else -> (it + dist).minValue(POSITIVE_INFINITY)
     }
 }
 
