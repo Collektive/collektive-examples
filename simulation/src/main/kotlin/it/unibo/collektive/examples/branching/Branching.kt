ef90c5b7e9d10f52b9e4e9b62f242d1fe98e61ad
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-24 15:16:29 +0200

    fix: remove use of deprecated functions

--- simulation/src/main/kotlin/it/unibo/collektive/examples/branching/Branching.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/branching/Branching.kt
@@ -1,14 +1,16 @@
 package it.unibo.collektive.examples.branching
 
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.neighboring
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
-import it.unibo.collektive.field.Field.Companion.hood
+import it.unibo.collektive.examples.neighbors.neighborCounter
 
 /**
  * A simple example of branching.
  */
-fun Aggregate<Int>.branching(environment: EnvironmentVariables) = when (environment.get<Boolean>("source")) {
-    true -> 0
-    false -> neighboring(1).hood(0) { acc, _ -> acc + 1 }
+fun Aggregate<Int>.branching(environment: EnvironmentVariables): Int {
+    val count = neighborCounter()
+    return when (environment.get<Boolean>("source")) {
+        true -> 0
+        false -> count
+    }
 }
--- simulation/src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/channel/ChannelWithObstacles.kt
@@ -2,11 +2,9 @@ package it.unibo.collektive.examples.channel
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.share
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.examples.gradient.gradient
-import it.unibo.collektive.field.Field.Companion.fold
-import kotlin.Double.Companion.POSITIVE_INFINITY
+import it.unibo.collektive.stdlib.spreading.gradientCast
 
 /**
  * Compute the channel between the source and the target with obstacles.
@@ -46,29 +44,34 @@ fun Aggregate<Int>.channel(
  * Computes the [gradientCast] from the [source] with the [value] that is the distance from the [source] to the target.
  */
 fun Aggregate<Int>.broadcast(distanceSensor: CollektiveDevice<*>, from: Boolean, payload: Double): Double =
-    gradientCast(distanceSensor, from, payload) { it }
+    gradientCast(
+        source = from,
+        local = payload,
+        metric = with(distanceSensor) { distances() },
+    )
+//        distanceSensor, from, payload) { it }
 
 /**
  * Compute the gradient of the aggregate from the [source] to the [target].
  * The [accumulate] function is used to accumulate the value of the aggregate.
  */
-fun Aggregate<Int>.gradientCast(
-    distanceSensor: CollektiveDevice<*>,
-    source: Boolean,
-    initial: Double,
-    accumulate: (Double) -> Double,
-): Double = share(POSITIVE_INFINITY to initial) { field ->
-    val dist = with(distanceSensor) { distances() }
-    when {
-        source -> 0.0 to initial
-        else -> {
-            val resultField =
-                dist.alignedMap(field) { distField, (currentDist, value) ->
-                    distField + currentDist to accumulate(value)
-                }
-            resultField.fold(POSITIVE_INFINITY to POSITIVE_INFINITY) { acc, value ->
-                if (value.first < acc.first) value else acc
-            }
-        }
-    }
-}.second
+//fun Aggregate<Int>.gradientCast(
+//    distanceSensor: CollektiveDevice<*>,
+//    source: Boolean,
+//    initial: Double,
+//    accumulate: (Double) -> Double,
+//): Double = share(POSITIVE_INFINITY to initial) { field ->
+//    val dist = with(distanceSensor) { distances() }
+//    when {
+//        source -> 0.0 to initial
+//        else -> {
+//            val resultField =
+//                dist.alignedMap(field) { distField, (currentDist, value) ->
+//                    distField + currentDist to accumulate(value)
+//                }
+//            resultField.fold(POSITIVE_INFINITY to POSITIVE_INFINITY) { acc, value ->
+//                if (value.first < acc.first) value else acc
+//            }
+//        }
+//    }
+//}.second
