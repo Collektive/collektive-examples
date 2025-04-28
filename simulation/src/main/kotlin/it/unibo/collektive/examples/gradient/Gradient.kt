1a0cb1e79c5113d8bc7d23b7b5c4ee2bd6fa97a9
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-04-28 20:03:39 +0200

    chore: add effects

--- simulation/src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/gradient/Gradient.kt
@@ -4,24 +4,32 @@ import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.Field
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.aggregate.api.share
-import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.stdlib.doubles.FieldedDoubles.plus
 import it.unibo.collektive.stdlib.fields.minValue
 import kotlin.Double.Companion.POSITIVE_INFINITY
 
 /**
- * Extension function to evaluate the gradient in an [Aggregate] context.
+ * Given the [distances] from the source, this function computes the gradient from the [source] to self.
  */
-fun Aggregate<Int>.gradient(distanceSensor: CollektiveDevice<*>, source: Boolean): Double = share(POSITIVE_INFINITY) {
-    val dist: Field<Int, Double> = with(distanceSensor) { distances() }
-    when {
-        source -> 0.0
-        else -> (it + dist).minValue(POSITIVE_INFINITY)
+fun Aggregate<Int>.gradient(
+    distances: Field<Int, Double>,
+    source: Boolean,
+): Double =
+    share(POSITIVE_INFINITY) { field ->
+        val minGradient = (field + distances).minValue(POSITIVE_INFINITY)
+        when {
+            source -> 0.0
+            else -> minGradient
+        }
     }
-}
 
 /**
- * The entrypoint of the simulation running a gradient.
+ * The entrypoint of the simulation running a gradient, considering the device with id 0 as the source.
  */
-fun Aggregate<Int>.gradientEntrypoint(environment: EnvironmentVariables, distanceSensor: CollektiveDevice<*>): Double =
-    gradient(distanceSensor, environment["source"])
+fun Aggregate<Int>.gradientEntrypoint(distanceSensor: CollektiveDevice<*>): Double =
+    with(distanceSensor) {
+        gradient(
+            distances = distances(),
+            source = localId == 0,
+        )
+    }
