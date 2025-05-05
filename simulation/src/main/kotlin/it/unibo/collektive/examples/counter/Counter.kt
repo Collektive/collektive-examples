aac343ce687f82c34a56dcc03db58fcf017ad543
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-05-05 10:12:32 +0200

    docs: improve grammar

--- simulation/src/main/kotlin/it/unibo/collektive/examples/counter/Counter.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/counter/Counter.kt
@@ -5,9 +5,8 @@ import it.unibo.collektive.stdlib.accumulation.countDevices
 
 /**
  * Count the number of devices in the network.
- * The total is accumulated in the device with [localId] 0
- * (where the [sink] is true).
- * Other devices will have a value that represents the number of devices
- * from self (included) to the closest edge of the network.
+ * The total is accumulated in the device with [localId] 0 (the [sink]).
+ * Other devices hold the number of devices in their subtree towards the network edge, inlcuding themselves.
+ * A leaf node (with no outward neighbors) will hold 1.
  */
 fun Aggregate<Int>.deviceCounter(): Int = countDevices(sink = localId == 0)
