918674550b155d6404af232c19e97df341ed5a55
Daniela Magrì <82176325+Danix002@users.noreply.github.com>
2025-05-09 09:46:38 +0200

    chore: style changes

--- simulation/src/main/kotlin/it/unibo/collektive/examples/exercises/NetworkDiameter.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/exercises/NetworkDiameter.kt
@@ -3,18 +3,19 @@ package it.unibo.collektive.examples.exercises
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.examples.channel.broadcast
-import it.unibo.collektive.stdlib.spreading.hopDistanceTo
 import it.unibo.collektive.stdlib.spreading.gossipMax
+import it.unibo.collektive.stdlib.spreading.hopDistanceTo
 
 /** Calculate in the source an estimate of the true diameter of the network
  * (the maximum distance of a device in the network).
  * Broadcast the diameter to every node in the network. */
 fun Aggregate<Int>.networkDiameter(distanceSensor: CollektiveDevice<*>): Int {
-    val isFurthest = isMax(distanceToSource(distanceSensor))
+    val distanceToSource = distanceToSource(distanceSensor)
+    val isFurthest = gossipMax(distanceToSource) == distanceToSource
     val distanceToFurthest = hopDistanceTo(isFurthest)
-    val flagNodeWithMaxHopToFurthest = isMax(distanceToFurthest)
+    val flagNodeWithMaxHopToFurthest = gossipMax(distanceToFurthest) == distanceToFurthest
     val broadcastMessage = broadcast(
-        with(distanceSensor) { distances() }, 
+        distances = with(distanceSensor) { distances() }, 
         from = flagNodeWithMaxHopToFurthest, 
         payload = distanceToFurthest.toDouble(),
     ).toInt()
@@ -24,8 +25,3 @@ fun Aggregate<Int>.networkDiameter(distanceSensor: CollektiveDevice<*>): Int {
     }
 }
 
-/** Function that identifies the maximum value and returns true if the passed value
- * is the maximum. */
-fun Aggregate<Int>.isMax(localValue: Int): Boolean = gossipMax(localValue).let { maxValue ->
-    localValue == maxValue
-}
