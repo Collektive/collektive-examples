fa9b6b24917096202258c4132c6498237c945a37
Danilo Pianini <danilo.pianini@unibo.it>
2025-05-14 12:14:20 +0200

    fix(simulation): more refinements to the simulated examples

--- simulation/src/main/kotlin/it/unibo/collektive/examples/spreading/MaxLocalId.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/spreading/MaxLocalId.kt
@@ -1,10 +1,12 @@
 package it.unibo.collektive.examples.spreading
 
 import it.unibo.collektive.aggregate.api.Aggregate
-import it.unibo.collektive.aggregate.api.mapNeighborhood
+import it.unibo.collektive.aggregate.api.neighborhood
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
-import it.unibo.collektive.stdlib.fields.maxValue
+import it.unibo.collektive.stdlib.fields.maxIDBy
+import it.unibo.collektive.stdlib.util.IncludingSelf
 
 /** Identify the maximum ID values among the neighboring nodes. */
 fun Aggregate<Int>.maxNeighborID(environment: EnvironmentVariables): Int =
-    mapNeighborhood { it }.maxValue(localId).also { environment["isMaxID"] = localId == it }
+    checkNotNull(neighborhood().maxIDBy(IncludingSelf) { it.id })
+        .also { environment["isMaxID"] = localId == it }
