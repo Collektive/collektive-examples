c6c92bec5148c0186a6a0bf223a5875be590b749
Daniela Magrì <daniela.adelina94@gmail.com>
2025-05-23 10:59:23 +0200

    chore: style changes

--- simulation/src/main/kotlin/it/unibo/collektive/examples/diameter/NetworkDiameter.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/diameter/NetworkDiameter.kt
@@ -1,9 +1,9 @@
 package it.unibo.collektive.examples.diameter
 
 import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.stdlib.spreading.distanceTo
 import it.unibo.collektive.stdlib.spreading.gossipMax
 import it.unibo.collektive.stdlib.spreading.gossipMin
-import it.unibo.collektive.stdlib.spreading.distanceTo
 import kotlin.random.Random
 
 /** Estimates the **diameter of the network** (i.e., the maximum
