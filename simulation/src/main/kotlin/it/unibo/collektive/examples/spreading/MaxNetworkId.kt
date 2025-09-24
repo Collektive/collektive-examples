3d47ed58698f4de5f8362ed669333b04474e73f6
Angela Cortecchia <angela.cortecchia@unibo.it>
2025-09-24 12:29:14 +0200

    fix: adapt to latest collektive version

--- simulation/src/main/kotlin/it/unibo/collektive/examples/spreading/MaxNetworkId.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/spreading/MaxNetworkId.kt
@@ -3,7 +3,8 @@ package it.unibo.collektive.examples.spreading
 import it.unibo.collektive.aggregate.api.Aggregate
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 import it.unibo.collektive.stdlib.spreading.gossipMax
+import it.unibo.collektive.stdlib.spreading.hopGossipMax
 
 /** Identify the maximum ID values in the network. */
 fun Aggregate<Int>.maxNetworkID(environment: EnvironmentVariables): Int =
-    gossipMax(localId).also { environment["isMaxID"] = localId == it }
+    hopGossipMax(localId).also { environment["isMaxID"] = localId == it }
