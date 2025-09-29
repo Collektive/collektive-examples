f7b5b8814deb043ea3dd9d78e43aef0e0b5f318f
Andrea Cecchini <cecchiniandrea@outlook.it>
2025-09-29 15:00:47 +0200

    refactor(temperature-example): improve the documentation and update the configuration to align with the MIT Proto example (except that the heat and cold sources are determined by the simulation environment)

--- simulation/src/main/kotlin/it/unibo/collektive/examples/temperature/Temperature.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/temperature/Temperature.kt
@@ -6,8 +6,8 @@ import it.unibo.collektive.aggregate.values
 import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
 
 private const val HEAT_SOURCE_TEMPERATURE = 30.0
-private const val COLD_SOURCE_TEMPERATURE = -20.0
-private const val INITIAL_TEMPERATURE = 0.0
+private const val COLD_SOURCE_TEMPERATURE = 20.0
+private const val INITIAL_TEMPERATURE = 25.0
 
 /**
  * Entry point for the program which computes the temperature's field.
@@ -16,8 +16,9 @@ fun Aggregate<Int>.temperatureEntrypoint(env: EnvironmentVariables): Double =
     temperature(env["heat_source"], env["cold_source"])
 
 /**
- * Represents an evolving temperature field where each device updates its temperature over time.
- * Some devices act as fixed heat sources at 30°C, others as fixed cold sources at -20°C.
+ * Represents an evolving pseudo-temperature field where each device updates its temperature over time.
+ * Some devices act as fixed heat sources ([heatSource]) at [HEAT_SOURCE_TEMPERATURE]°C,
+ * others as fixed cold sources ([coldSource]) at [COLD_SOURCE_TEMPERATURE]°C.
  * All other devices calculate their temperature dynamically as the average temperature of their neighboring devices.
  */
 fun Aggregate<Int>.temperature(heatSource: Boolean, coldSource: Boolean): Double =
