4a96e811ff0968833deb2d8b72bc3c98098ce2c1
Andrea Cecchini <63281186+andreacecchini@users.noreply.github.com>
2025-10-09 14:31:17 +0200

    feat: implement MIT-Proto in-circle demo  (#565)
    * feat(feat/in-circle): added in-circle example
    
    * refactor(feat/in-circle): Added doc for  and
    
    * refactor(in-circle): refactor inCircle function to use Point2D
    
    * refactor(feat\in-circle): rename parameter 'p' to 'location' in inCircle functions
    
    * fix(feat\in-circle): fix too long lines.
    
    * fix(feat\in-circle): fix format problems

---
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/inCircle/InCircle.kt
@@ -0,0 +1,53 @@
+package it.unibo.collektive.examples.inCircle
+
+import it.unibo.alchemist.collektive.device.CollektiveDevice
+import it.unibo.alchemist.model.Position
+import it.unibo.collektive.aggregate.Field
+import it.unibo.collektive.aggregate.api.Aggregate
+import it.unibo.collektive.alchemist.device.sensors.EnvironmentVariables
+import it.unibo.collektive.stdlib.spreading.gradientCast
+import it.unibo.collektive.stdlib.util.Point2D
+import kotlin.math.pow
+
+private const val RADIUS = 30.0
+
+/**
+ * Entry point for the program which computes whether the device is within a circle of a given radius.
+ */
+fun Aggregate<Int>.inCircleEntrypoint(env: EnvironmentVariables, collektiveDevice: CollektiveDevice<*>): Boolean =
+    with(collektiveDevice) {
+        inCircle(
+            center = env["center"],
+            location = environment.getPosition(collektiveDevice.node).toPoint2D(),
+            metric = { distances() },
+        )
+    }
+
+/**
+ * Determines if the current device (located in [location])
+ * is within a circle of a specified radius from a [center] point.
+ */
+fun Aggregate<Int>.inCircle(center: Boolean, location: Point2D, metric: () -> Field<Int, Double>): Boolean =
+    with(location) {
+        // Broadcast the center position to the whole network
+        val centerPos = gradientCast(
+            source = center,
+            local = this,
+            metric = metric(),
+        )
+        distanceToSquared(centerPos) <= RADIUS.pow(2)
+    }
+
+/**
+ * Computes the squared distance between this point and [other] point.
+ */
+private fun Point2D.distanceToSquared(other: Point2D): Double {
+    val dx = x - other.x
+    val dy = y - other.y
+    return dx * dx + dy * dy
+}
+
+/**
+ * Converts a [Position] to a [Point2D] collektive type.
+ */
+private fun Position<*>.toPoint2D(): Point2D = Point2D(coordinates[0] to coordinates[1])
