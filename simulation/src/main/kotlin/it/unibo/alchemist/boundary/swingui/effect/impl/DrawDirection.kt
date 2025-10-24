54894a62b30332605c6ec060a2f0fadfe057b514
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2025-10-24 07:01:08 +0200

    refactor(feat/track): fix Effect import

--- simulation/src/main/kotlin/it/unibo/alchemist/boundary/swingui/effect/impl/DrawDirection.kt
+++ simulation/src/main/kotlin/it/unibo/alchemist/boundary/swingui/effect/impl/DrawDirection.kt
@@ -1,5 +1,8 @@
+@file:Suppress("DEPRECATION")
+
 package it.unibo.alchemist.boundary.swingui.effect.impl
 
+import it.unibo.alchemist.boundary.swingui.effect.api.Effect
 import it.unibo.alchemist.boundary.ui.api.Wormhole2D
 import it.unibo.alchemist.model.Environment
 import it.unibo.alchemist.model.Node
@@ -20,8 +23,7 @@ import kotlin.math.sqrt
 /**
  * Effect that draws a unit direction vector as an arrow from the node's position.
  */
-@Suppress("DEPRECATION")
-class DrawDirection(private val c: Color = Color.RED) : it.unibo.alchemist.boundary.swingui.effect.api.Effect {
+class DrawDirection(private val c: Color = Color.RED) : Effect {
 
     override fun getColorSummary(): Color = c
 
