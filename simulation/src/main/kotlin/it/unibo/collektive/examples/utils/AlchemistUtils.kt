61e6b53538cc46bdf9ca5309cd2cb76937589986
Andrea Cecchini <63281186+andreacecchini@users.noreply.github.com>
2025-10-24 16:14:42 +0200

    feat: implement MIT-Proto track demo (#575)

--- simulation/src/main/kotlin/it/unibo/collektive/examples/utils/AlchemistUtils.kt
+++ simulation/src/main/kotlin/it/unibo/collektive/examples/utils/AlchemistUtils.kt
@@ -2,6 +2,8 @@ package it.unibo.collektive.examples.utils
 
 import it.unibo.alchemist.collektive.device.CollektiveDevice
 import it.unibo.alchemist.model.Position
+import it.unibo.alchemist.model.physics.environments.Physics2DEnvironment
+import it.unibo.alchemist.model.positions.Euclidean2DPosition
 import it.unibo.collektive.stdlib.util.Point2D
 
 /**
@@ -23,3 +25,15 @@ fun CollektiveDevice<*>.move(direction: Vector2D, velocity: Double) {
     val newPos = coordinates() + (direction * velocity)
     environment.moveNodeToPosition(node, environment.makePosition(newPos.x, newPos.y))
 }
+
+/**
+ * Sets the heading of the device in the environment towards a given [direction].
+ * The environment where the device is located must be a [Physics2DEnvironment].
+ */
+fun CollektiveDevice<*>.pointTo(direction: Vector2D) = (environment as? Physics2DEnvironment<Any?>)
+    ?.setHeading(node, direction.toEuclidean2DPosition())
+
+/**
+ * Converts a [Vector2D] to an [Euclidean2DPosition].
+ */
+fun Vector2D.toEuclidean2DPosition(): Euclidean2DPosition = Euclidean2DPosition(x, y)
